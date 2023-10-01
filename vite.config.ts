b2431361ffd892a31e3b410f54546203ed437d7f
Alex MacArthur <alex@macarthur.me>
2023-10-01 00:50:00 -0500

    use static path for assets.

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import solidPlugin from "vite-plugin-solid";
 // import devtools from 'solid-devtools/vite';
 
 export default defineConfig({
+  base: "https://headlights.vercel.app",
   plugins: [
     /* 
     Uncomment the following line to enable solid-devtools.
