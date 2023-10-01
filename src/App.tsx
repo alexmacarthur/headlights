0d1c7d1088a75aacc7a183763d2830c165c8ab02
Alex MacArthur <alex@macarthur.me>
2023-10-01 14:55:24 -0500

    Update meta, CSS.

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ const App: Component = () => {
     <div class={"px-3 pt-14 pb-6 min-h-screen flex flex-col relative"}>
       <div class="flex-1 flex flex-col justify-center">
         <div>
-          <h1 class="text-2xl md:text-4xl text-center mb-10">
+          <h1 class="text-2xl md:text-4xl text-center mb-12 max-w-lg mx-auto">
             View an HTTP Request's Response Headers
           </h1>
 
